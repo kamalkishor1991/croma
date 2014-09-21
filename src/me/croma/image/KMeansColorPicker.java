c96b7525ce36055158cca7a1eaaa56cd7fc2dbb8
kamal <kkjoshi.it@gmail.com>
2014-09-21 22:42:26 +0530

    new distance calculation based on LAB color space.

--- src/me/croma/image/KMeansColorPicker.java
+++ src/me/croma/image/KMeansColorPicker.java
@@ -2,10 +2,7 @@ package me.croma.image;
 
 import me.croma.ml.KMeansClustering;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
+
 import java.io.IOException;
 import java.util.*;
 import java.util.List;
@@ -14,24 +11,26 @@ import java.util.List;
  * Using KMeans algorithm for clustering colors.
  */
 public class KMeansColorPicker implements ColorPicker {
-
+    public KMeansColorPicker() {
+        //default constructor
+    }
 	@Override
 	public List<Color> getUsefulColors(Image img, int noOfColors) throws IOException {
         int mx = Math.max(img.getHeight(), img.getWidth());
         int mn = Math.min(img.getHeight(), img.getWidth());
-        int sh = Math.min(400, mx);
+        int sh = Math.min(100, mx);
         int sw = Math.min((int)(sh * (mn * 1.0)/mx), mn);
         Image image = img.getScaledInstance(sh, sw);
 		int h = image.getHeight();
 		int w = image.getWidth();
-
 		double input[][] = new double[h*w][3];
+        ColorSpaceConverter cs = new ColorSpaceConverter();
 		int index = 0;
 		for (int i = 0;i < w;i++) {
 			for (int j = 0;j < h;j++) {
 				int rgb = image.getColor(i,j).getRGB();
                 Color c = new Color(rgb);
-                float f[] = {c.getRed(), c.getGreen(), c.getBlue()};
+                double f[] = cs.RGBtoLAB(c.getRed(), c.getGreen(), c.getBlue());
                 input[index][0] = f[0];
                 input[index][1] = f[1];
                 input[index++][2] = f[2];
@@ -41,34 +40,17 @@ public class KMeansColorPicker implements ColorPicker {
 		km.iterate(20);
 		double m[][] = km.getMeans();
 		List<Color> r = new ArrayList<Color>(noOfColors);
-
-        for (double c[] : m) {
-            r.add(new Color((int)c[0], (int)c[1],(int) c[2]));
-		}
-        List<Color> ret = new ArrayList<Color>();
         Set<Color> set = new HashSet<Color>();
-        for (Color c : r) {
-            double min[] = input[0];
-            double ct[] = {c.getRed(), c.getGreen(), c.getBlue()};
-            double dis = Double.MAX_VALUE;
-            for (int i = 0; i < input.length; i++) {
-                if (distance(input[i] , ct ) < dis) {
-                    dis = distance(input[i] , ct );
-                    min = input[i];
-                }
-            }
-            Color cc = new Color((int)min[0], (int)min[1], (int)min[2]);
-            if (!set.contains(cc)) {
-                ret.add(cc);
-            }
+        for (double c[] : m) {
+            int ct[] = cs.LABtoRGB(c);
+            Color cc = new Color(ct[0], ct[1], ct[2]);
+            if (!set.contains(cc)) r.add(cc);
             set.add(cc);
-        }
-
-		return ret;
+		}
+		return r;
 
 		//System.out.println(Arrays.toString(ans));
 	}
-
 
     /**
      * Euclidean distance
